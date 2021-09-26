0c1b63eebd5efc533b29103fb38a0fcfae39c455
Shanila <88085935+IT20225506@users.noreply.github.com>
2021-09-26 23:11:49 +0530

    Notes and Indicator 1

---
+++ app/src/main/java/com/example/carserviceapp/Indicators.java
@@ -0,0 +1,31 @@
+package com.example.carserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Indicators extends AppCompatActivity {
+
+    public Button buttonW;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indicators);
+
+        buttonW = (Button) findViewById(R.id.button8);
+        buttonW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Indicators.this,Warning.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/carserviceapp/Notes.java
@@ -0,0 +1,16 @@
+package com.example.carserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+
+    }
+}
